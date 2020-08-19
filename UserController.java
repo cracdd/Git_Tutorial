9164e59972077416608623b14ae882eed7a4f7f5
cracdd <cracdd@gmail.com>
2020-08-19 12:40:42 +0900

    Add UserController [Update]

--- UserController.java
+++ UserController.java
@@ -12,6 +12,7 @@ public class UserController {
 		logger.info("login: insert");
 		return "user/insert";
 	}
+	//회원 정보 수정 페이지로 이동
 	@RequestMapping("/user/update.do")
 	public String update() {
 		logger.info("login: update");
